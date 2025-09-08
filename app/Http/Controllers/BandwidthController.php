a2a422a9d88f7729f1aad66db2ff767863dad50a
Faiz-Akbar-Putra-Wardani <faizbot81@gmail.com>
2025-09-08 14:37:16 +0700

    first commit

---
+++ app/Http/Controllers/BandwidthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bandwidth;
+use Illuminate\Http\Request;
+
+class BandwidthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Bandwidth $bandwidth)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Bandwidth $bandwidth)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Bandwidth $bandwidth)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Bandwidth $bandwidth)
+    {
+        //
+    }
+}
