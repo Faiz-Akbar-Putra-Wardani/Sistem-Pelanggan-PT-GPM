34ba46735d6acddf842e8e022a24f3dfae5cbcfb
Miezan El Khoir <154299096+miezelkhr@users.noreply.github.com>
2025-09-12 11:23:44 +0700

    menambahkan sweet alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+import './alert.js';
+import './alert-data-master.js';
