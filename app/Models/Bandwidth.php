8fcaf89133bd9e13e0c840d536bf7075bbd30566
AlvinBrianda <alvinbrianda23@gmail.com>
2025-09-12 14:09:28 +0700

    Update

--- app/Models/Bandwidth.php
+++ app/Models/Bandwidth.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bandwidth extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'bandwidths';
+
+    protected $fillable = [
+        'nilai',
+    ];
+
+    public static function Options()
+    {
+        return [
+            '5 Mbps',
+            '10 Mbps',
+            '20 Mbps',
+            'Lainnya',
+        ];
+    }
+
+
+    public function paketInternet()
+    {
+        return $this->belongsToMany(PaketInternet::class, 'paket_bandwidth', 'bandwidth_id', 'paket_internet_id');
+    }
+
+
 }
