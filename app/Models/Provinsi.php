4f5272d76dcd650c2545709c202c3d7b3f7f528f
Miezan El Khoir <154299096+miezelkhr@users.noreply.github.com>
2025-09-12 14:32:49 +0700

    update model provinsi

--- app/Models/Provinsi.php
+++ app/Models/Provinsi.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Carbon\Traits\Timestamp;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Provinsi extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'provinsis';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public function kabupaten()
+    {
+        return $this->hasMany(Kabupaten::class);
+    }
 }
