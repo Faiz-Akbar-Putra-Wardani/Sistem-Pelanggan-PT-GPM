b0864c9691e7265bfc77aeacb6832f7f5a758c45
Faiz-Akbar-Putra-Wardani <faizbot81@gmail.com>
2025-09-12 15:20:57 +0700

    update table paket internet

--- database/migrations/2025_09_08_073233_create_paket_internets_table.php
+++ database/migrations/2025_09_08_073233_create_paket_internets_table.php
@@ -9,17 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('paket_internets', function (Blueprint $table) {
+    public function up()
+{
+    Schema::create('paket_internets', function (Blueprint $table) {
         $table->id();
         $table->string('nama_paket', 150)->nullable();
         $table->string('paket_internet', 100)->nullable();
-        $table->decimal('harga_bulanan', 10, 2)->nullable();
+        $table->decimal('harga_bulanan', 10, 2);
         $table->boolean('is_active')->default(true);
         $table->timestamps();
-        });
-    }
+    });
+}
 
     /**
      * Reverse the migrations.
