cf20e5bae3d731bd15a721de571b909cf1509653
Muhammad Diaz Alwi <alwidiaz502@gmail.com>
2025-09-12 11:41:23 +0700

    update store paket internet

---
+++ app/Http/Requests/UpdatePaketInternet.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\PaketInternet;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePaketInternet extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+        {
+            return [
+                'nama_paket' => ['sometimes', 'string', 'max:150'],
+                'paket_internet' => ['sometimes', 'string', 'in:' . implode(',', PaketInternet::Paket_Internet)],
+                'harga_bulanan' => ['sometimes',  'numeric', 'min:0'],
+                'is_active' => ['sometimes', 'boolean'],
+            ];
+        }
+
+
+    public function messages(): array
+    {
+        return [
+
+            'paket_internet.in' => 'Paket internet tidak valid.',
+
+            'harga_bulanan.numeric' => 'Harga bulanan harus berupa angka.',
+            'harga_bulanan.min' => 'Harga bulanan tidak boleh kurang dari 0.',
+
+            'is_active.boolean' => 'Status aktif harus berupa true/false.',
+        ];
+    }
+
+}
