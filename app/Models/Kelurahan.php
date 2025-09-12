a1f19f79362f23a895c95e18b2593304edb542f5
Miezan El Khoir <154299096+miezelkhr@users.noreply.github.com>
2025-09-12 14:35:07 +0700

    update model kelurahan

--- app/Models/Kelurahan.php
+++ app/Models/Kelurahan.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kelurahan extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'kelurahans';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public function kecamatan()
+    {
+        return $this->belongsTo(Kecamatan::class);
+    }
+
+
 }
