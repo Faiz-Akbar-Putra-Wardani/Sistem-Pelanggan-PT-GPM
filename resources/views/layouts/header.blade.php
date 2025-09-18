ac7f690594104cd5135dce6e796b6d39bbe48bb9
Faiz-Akbar-Putra-Wardani <faizbot81@gmail.com>
2025-09-18 09:58:16 +0700

    update header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,35 +12,14 @@
                             <i class="ti ti-menu-2"></i>
                         </a>
                     </li>
-                    <li class="dropdown pc-h-item d-inline-flex d-md-none">
-                        <a class="pc-head-link head-link-primary dropdown-toggle arrow-none m-0"
-                           data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
-                           aria-expanded="false">
-                            <i class="ti ti-search"></i>
-                        </a>
-                        <div class="dropdown-menu pc-h-dropdown drp-search">
-                            <form class="px-3">
-                                <div class="form-group mb-0 d-flex align-items-center">
-                                    <i data-feather="search"></i>
-                                    <input type="search" class="form-control border-0 shadow-none" name="search" placeholder="Cari...">
-                                </div>
-                            </form>
-                        </div>
-                    </li>
-                    <li class="pc-h-item d-none d-md-inline-flex">
-                        <form class="header-search">
-                            <i data-feather="search" class="icon-search"></i>
-                            <input type="search" class="form-control" name="search" placeholder="Cari...">
-                        </form>
-                    </li>
                 </ul>
             </div>
-            <div class="ms-auto">
+           <div class="ms-auto">
                 <ul class="list-unstyled">
                     <li class="dropdown pc-h-item header-user-profile">
                         <a class="pc-head-link head-link-primary dropdown-toggle arrow-none me-0"
-                           data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
-                           aria-expanded="false">
+                            data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
+                            aria-expanded="false">
                             <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}&background=1C76DA&color=fff" alt="user-image" class="user-avtar">
                             <span>
                                 <i class="ti ti-settings"></i>
@@ -48,14 +27,12 @@
                         </a>
                         <div class="dropdown-menu dropdown-user-profile dropdown-menu-end pc-h-dropdown">
                             <div class="dropdown-header">
-                                <h4><span class="small text-muted"> {{ Auth::user()->name }}</span></h4>
-                                <p class="text-muted">{{ Auth::user()->name }}</p>
+                                <h4> <span class="small text-muted"> {{ Auth::user()->name }}</span></h4>
                                 <hr>
                                 <div class="profile-notification-scroll position-relative" style="max-height: calc(100vh - 280px)">
-                                    <form method="POST" action="{{ route('logout') }}">
+                                    <form action="{{ route('logout') }}" method="POST">
                                         @csrf
-                                        <button type="submit" class="dropdown-item"
-                                                onclick="return confirm('Apakah anda yakin ingin mengakhiri sesi ini?')">
+                                        <button type="submit" class="dropdown-item">
                                             <i class="ti ti-logout"></i>
                                             <span>Keluar</span>
                                         </button>
